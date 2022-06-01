d124313982979c74a8578db6488a2093af13da9f
Rodrigo Torres <roandrestorresm@gmail.com>
2022-06-01 16:30:36 -0400

    Kernel modificado

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
             'throttle:api',
